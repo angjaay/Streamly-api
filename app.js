ebaeddf797ac4183a2f4e1f8bb418b20ed0c3dd5
angjaay <angga.angzai@gmail.com>
2023-02-09 17:35:20 +0700

    ad cors

--- app.js
+++ app.js
@@ -41,12 +41,21 @@ if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'))
 }
 
-app.use(
-	helmet({
-	  crossOriginEmbedderPolicy: false,
-	  // ...
-	})
-  );
+// app.use(
+// 	helmet({
+// 	  crossOriginEmbedderPolicy: false,
+// 	  // ...
+// 	})
+//   );
+
+
+const addCorsHeaders = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+};
+
+app.use(addCorsHeaders);
 
 // File uploading
 app.use(
